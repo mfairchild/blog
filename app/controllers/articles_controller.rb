31b57e46aaa14fd567d060ea5bc4545a70f6e970
Mark Fairchild <mfairchild@medicaldocuments.com>
2017-07-22 14:48:18 -0400

    Added destroy functionality and simple authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,26 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "mf", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -17,26 +28,25 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
   def update
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
 
-  if @article.update(article_params)
-    redirect_to @article
-  else
-    render 'edit'
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
-end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
